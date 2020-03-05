6018989689d3391b8f0c105abbc38e6c62d25d41
ZachoryStrike <zstrike@carecloud.com>
2020-03-04 20:20:48 -0500

    Refactor Viewer

--- src/skills/viewer/Viewer.jsx
+++ src/skills/viewer/Viewer.jsx
@@ -5,72 +5,68 @@ import Arrow from '../../home/Arrow.svg';
 const Viewer = () => {
     const [currentSlide, setSlide] = useState(0);
 
-    const carecloudEngineer = [`Lead feature team of 10 (8 engineers, 2 QA) in building front office portion of practice management software`,
-    `Architect Backend Data Models/Orchestration with Microservices`,
-    `Golang, C#, JS, Graphql, Netflix Orchestrator, FHIR`,
-    `Implement and update UI features throughout configuration and booking views in React within Micro frontend architecture`,
-    `Mentor direct reports across other feature teams`];
-    const carecloudAutomation = [`Lead team of 11 engineers`,
-    `Architected Automation Dashboard: Front/Backend in React/Graphql/Node.js`,
-    `Implemented and oversaw multiple automation frameworks (UI/API testing)`,
-    `Created RPA Dashboard in React with Graphql Backend Server`,
-    `Hired/Mentored team of junior developers`];
-    const JCI = [`Lead testing team for GLAS Thermostat project`,
-    `Device/Web/Mobile testing`,
-    `SQA Automation`];
-    const Advicent = [`Implemented WCF services in C#`, `Wrote Specflow tests to verify database responses`, `Integration tests written in specflow to validate services`];
+    const carecloudEngineer = {
+        company: 'CareCloud',
+        position: 'Lead Software Engineer II',
+        bullets: [`Lead feature team of 10 (8 engineers, 2 QA) in building front office portion of practice management software`,
+        `Architect Backend Data Models/Orchestration with Microservices`,
+        `Golang, C#, JS, Graphql, Netflix Orchestrator, FHIR`,
+        `Implement and update UI features throughout configuration and booking views in React within Micro frontend architecture`,
+        `Mentor direct reports across other feature teams`],
+    };
+
+    const carecloudAutomation = {
+        company: 'CareCloud Cont.',
+        position: 'Lead Software Engineer in Test',
+        bullets: [`Lead team of 11 engineers`,
+        `Architected Automation Dashboard: Front/Backend in React/Graphql/Node.js`,
+        `Implemented and oversaw multiple automation frameworks (UI/API testing)`,
+        `Created RPA Dashboard in React with Graphql Backend Server`,
+        `Hired/Mentored team of junior developers`],
+    };
+
+    const JCI = {
+        company: 'SafeNet Consulting',
+        position: 'IT Consultant',
+        bullets: [`Lead testing team for GLAS Thermostat project`,
+        `Device/Web/Mobile testing`,
+        `SQA Automation`],
+    };
+
+    const Advicent = {
+        company: 'Advicent',
+        position: 'QA Engineer',
+        bullets: [`Implemented WCF services in C#`, `Wrote Specflow tests to verify database responses`, `Integration tests written in specflow to validate services`],
+    };
+
     const slides = [
         carecloudEngineer, carecloudAutomation, JCI, Advicent
     ];
 
     const clickRight = _ => {
-        if (currentSlide < 3) {
-            setSlide(currentSlide + 1)
-        } else {
-            setSlide(0)
-        }
-    }
+        currentSlide < 3 ? setSlide(currentSlide + 1) : setSlide(0);
+    };
     const clickLeft = _ => {
-        if (currentSlide > 0) {
-            setSlide(currentSlide - 1)
-        } else {
-            setSlide(slides.length - 1)
-        }
-    }
-
+        currentSlide > 0 ? setSlide(currentSlide - 1) : setSlide(slides.length - 1);
+    };
 
     return (
         <div className={classes.ViewerContainer}>
             <div className={classes.Viewer}>
-                <div className={classes.ViewerBox} style={{ transform: `translate(${currentSlide * -450}px)` }} >
-                    <div className={classes.Slide}>
-                        <h2 className={classes.Header}>CareCloud</h2>
-                        <h2 className={classes.Header}>Lead Software Engineer II</h2>
-                        <ul className={classes.Text}>
-                            {slides[0].map(bullet => <li>{bullet}</li>)}
-                        </ul>
-                    </div>
-                    <div className={classes.Slide}>
-                        <h2 className={classes.Header}>CareCloud Cont.</h2>
-                        <h2 className={classes.Header}>Lead Software Engineer in Test</h2>
-                        <ul className={classes.Text}>
-                            {slides[1].map(bullet => <li>{bullet}</li>)}
-                        </ul>
-                    </div>
-                    <div className={classes.Slide}>
-                        <h2 className={classes.Header}>SafeNet Consulting</h2>
-                        <h2 className={classes.Header}>IT Consultant</h2>
-                        <ul className={classes.Text}>
-                            {slides[2].map(bullet => <li>{bullet}</li>)}
-                        </ul>
-                    </div>
-                    <div className={classes.Slide}>
-                        <h2 className={classes.Header}>Advicent</h2>
-                        <h2 className={classes.Header}>QA Engineer</h2>
-                        <ul className={classes.Text}>
-                            {slides[3].map(bullet => <li>{bullet}</li>)}
-                        </ul>
-                    </div>
+                <div className={classes.ViewerBox} style={{ transform: `translate(${currentSlide * -450}px)`, width: `${slides.length * 100}%` }} >
+                    {
+                        slides.map(profile => {
+                            return (
+                                <div className={classes.Slide}>
+                                    <h2 className={classes.Header}>{profile.company}</h2>
+                                    <h2 className={classes.Header}>{profile.position}</h2>
+                                    <ul className={classes.Text}>
+                                        {profile.bullets.map(bullet => <li>{bullet}</li>)}
+                                    </ul>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <div className={classes.ViewerNavigation}>
